15c89ee0ff8309319af991fa5061e1ec137391cc
EXuanPang <e.xuan@hotmail.com>
2024-08-16 18:47:12 +0900

    feat(ex):Completed exercises

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalAmount = 27000
+let discount = totalAmount * 25/100
+let afterDicount = totalAmount - discount
+console.log(afterDicount)
